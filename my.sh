f1d3ddcd5db7967caa834e871b605dd48637b8cf
xwqiang <642143714@qq.com>
2014-07-02 16:45:54 +0800

    init

--- my.sh
+++ my.sh
@@ -1,39 +1,31 @@
-#!/bin/bash
-#用来匹配的列
-_command="Command:";
-#期望的值
-_expect="Sleep"
-#需要显示的列
-_dest='Info:'
-
-function getItem(){
-	mysql -uroot -p1 -e 'show processlist\G'| grep -A 8 "* $1. row"| grep -v "* $1. row"| grep $2|xargs;
-}
-
-function getSum(){
-	echo `mysql -uroot -p1 -e 'show processlist'| wc -l`
-}
-
-tmp=$(getSum);
-_sum=$(($tmp-1));
-echo $_sum;
-
-for((i=1;i<$_sum;i++));do
-
-
-#	echo "command:$_command";
-	result=$(getItem "$i" "$_command");
-#	echo "result-->$result";
-	msg=$(echo $result | awk -F ': ' '{print $2}');
-#	echo "msg-->$msg";
-#	echo "expected-->$_expect";
-	if [ "$msg" == "$_expect" ];then
-#	echo "aa";
-#	echo $msg;
-	echo $(getItem "$i" "$_dest");
-#	else
-#	echo '....';
-	fi
-
-
-done;
+#!/bin/sh
+# description: Start LVS of Director server
+VIP=192.168.12.135
+RIP1=192.168.12.246
+RIP2=192.168.12.237
+/etc/rc.d/init.d/functions
+case "$1" in
+    start)
+        echo " start LVS of Director Server"
+# set the Virtual  IP Address and sysctl parameter
+ /sbin/ifconfig eth0:0 $VIP broadcast $VIP netmask 255.255.255.255 up
+       echo "1" >/proc/sys/net/ipv4/ip_forward 
+#Clear IPVS table
+       /sbin/ipvsadm -C
+#set LVS
+/sbin/ipvsadm -A -t $VIP:80 -s rr -p 600
+/sbin/ipvsadm -a -t $VIP:80 -r $RIP1:80 -g
+/sbin/ipvsadm -a -t $VIP:80 -r $RIP2:80 -g
+#Run LVS
+      /sbin/ipvsadm
+       ;;
+    stop)
+        echo "close LVS Directorserver"
+        echo "0" >/proc/sys/net/ipv4/ip_forward
+        /sbin/ipvsadm -C
+        /sbin/ifconfig eth0:0 down
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+esac
